64a27571dac4357db96bfaa01b657f16dc73adb5
RSGinScorpii <aaronkeith_angustia@aura.edu.ph>
2024-01-19 15:28:24 +0800

    before new changes

--- src/app/features/main/dashboard/dashboard.component.ts
+++ src/app/features/main/dashboard/dashboard.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  NgbDateStruct,
+  NgbDate,
+  NgbCalendar,
+  NgbDateParserFormatter,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  model: NgbDateStruct;
+  dayTemplateData: { date: NgbDate; current: boolean };
 
+  // Add this property
+  customDayTemplate: any;
+
+  constructor(
+    private calendar: NgbCalendar,
+    private formatter: NgbDateParserFormatter
+  ) {
+    this.model = this.calendar.getToday();
+    this.dayTemplateData = { date: this.calendar.getToday(), current: false };
+  }
+
+  get dayTemplate() {
+    return this.dayTemplateData.current ? this.customDayTemplate : undefined;
+  }
 }
--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, NgbModule, NgbDatepickerModule],
   exports: [DashboardComponent],
 })
 export class MainModule {}

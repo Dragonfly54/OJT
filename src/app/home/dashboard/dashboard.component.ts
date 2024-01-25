7203abb7dba6597810a13e0b9a53c5f3f64f7570
RSGinScorpii <aaronkeith_angustia@aura.edu.ph>
2024-01-25 22:57:50 +0800

    added clock

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { Observable, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 import {
   NgbDateStruct,
   NgbDate,
@@ -11,11 +14,13 @@ import {
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   model: NgbDateStruct;
   dayTemplateData: { date: NgbDate; current: boolean };
+  clockHour$!: Observable<string>;
+  clockMinute$!: Observable<string>;
+  clockText$!: Observable<any>;
 
-  // Add this property
   customDayTemplate: any;
 
   constructor(
@@ -28,5 +33,50 @@ export class DashboardComponent {
 
   get dayTemplate() {
     return this.dayTemplateData.current ? this.customDayTemplate : undefined;
+  }
+
+  ngOnInit(): void {
+    this.clockHour$ = interval(1000).pipe(map(() => this.getClockTime('hour')));
+
+    this.clockMinute$ = interval(1000).pipe(
+      map(() => this.getClockTime('minute'))
+    );
+
+    this.clockText$ = interval(1000).pipe(map(() => this.getClockText()));
+  }
+
+  private getClockTime(unit: 'hour' | 'minute'): string {
+    const date = moment();
+    const hh = date.hours() % 12;
+    const mm = date.minutes();
+    const hourAngle = (hh + mm / 60) * 30;
+    const minuteAngle = mm * 6;
+
+    return unit === 'hour'
+      ? `rotateZ(${hourAngle}deg)`
+      : `rotateZ(${minuteAngle}deg)`;
+  }
+
+  private getClockText(): any {
+    const date = moment();
+    const dayWeek = date.format('dddd');
+    const month = date.format('MMM');
+    const day = date.format('D');
+    const year = date.format('YYYY');
+    let hh = date.format('h');
+    const mm = date.format('mm');
+    let ampm = date.format('A');
+
+    hh = hh === '0' ? '12' : hh;
+
+    return {
+      dayWeek,
+      month,
+      day: ` ${day}, `,
+      year,
+      ampm,
+      textHour: `${hh}:`,
+      textMinutes: mm,
+    };
   }
 }
